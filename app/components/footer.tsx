6e277df7add1129520d4ae2d298b9b92d44bd9de
milliorn <scottmilliorn@gmai.com>
2023-07-06 01:21:41 -0700

    footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,26 +2,37 @@ import React from "react";
 import { Envelope, Linkedin, Medium, Twitter } from "../icons";
 
 type FooterProps = {
-	contactMeLinks: string[];
+  contactMeLinks: string[];
 };
 
-export const Footer = ({ contactMeLinks }: FooterProps) => {
-	return (
-		<footer className='w-full bg-white shadow-footer mt-12 py-4 fixed bottom-0 '>
-			<div className='flex items-center justify-center gap-1'>
-				<a href={contactMeLinks[0]} aria-label='Email'>
-					<Envelope className='icons_contactme' />
-				</a>
-				<a href={contactMeLinks[1]} aria-label='Twitter'>
-					<Twitter className='icons_contactme' />
-				</a>
-				<a href={contactMeLinks[2]} aria-label='Linkedin'>
-					<Linkedin className='icons_contactme' />
-				</a>
-				<a href={contactMeLinks[3]} aria-label='Medium'>
-					<Medium className='icons_contactme' />
-				</a>
-			</div>
-		</footer>
-	);
-};
+// Footer component displaying social media icons
+export function Footer({ contactMeLinks }: FooterProps): JSX.Element {
+  // Array of social media icons with their labels
+  const socialIcons = [
+    { icon: <Envelope className="icons_contactme" />, label: "Email" },
+    { icon: <Linkedin className="icons_contactme" />, label: "Linkedin" },
+    { icon: <Medium className="icons_contactme" />, label: "Medium" },
+    { icon: <Twitter className="icons_contactme" />, label: "Twitter" },
+  ];
+
+  return (
+    // Footer container
+    <footer className="w-full bg-white shadow-footer mt-12 py-4 fixed bottom-0 ">
+      {/* Container for social media icons */}
+      <div className="flex items-center justify-center gap-1">
+        {/* Map over the socialIcons array and render each icon */}
+        {socialIcons.map((socialIcon, index) => (
+          // Anchor element linking to the respective social media
+          <a
+            href={contactMeLinks[index]}
+            aria-label={socialIcon.label}
+            key={index}
+          >
+            {/* Render the social media icon */}
+            {socialIcon.icon}
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+}
