49648cd6741777c1b84f1e2346174e3a192e6bf4
milliorn <scottmilliorn@gmail.com>
2024-01-03 16:00:20 -0800

    Update head.tsx with meta tags and title

--- app/head.tsx
+++ app/head.tsx
@@ -3,9 +3,10 @@ import React from "react";
 export default function LayoutHead() {
   return (
     <>
-      <title>NextJS Mongo Starter</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Starter template for NextJS 13 with Mongo Database" />
+      <title>NextJS Mongo Starter</title>
     </>
   );
 }
